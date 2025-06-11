788cba87dc90a3e9e2d1d560655dedacc2038bf1
Con-Adonis <conkavleski@gmail.com>
2025-06-11 14:55:20 -0400

    Page fixing - Style initialization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 export default function Home(){
   return(
-    <main className = "min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className = "text-4xl text-gray-800 font-bold mb-5">Sample Text</h1>
-      <p className="text-lg text-gray-700">Sample paragraph</p>
+    <main className="min-h-screen bg-white pl-16 pt-8">
+      <h1 className="text-5xl text-black mb-6">Sample title</h1>
+      <p className="text-black mb-6">Sample paragraph</p>
+      <button className="px-3 py-1 bg-white text-black border border-2 border-black rounded-full cursor-pointer transition duration-350 hover:scale-99 hover:bg-black hover:text-white">Click me</button>
     </main>
   );
 }
