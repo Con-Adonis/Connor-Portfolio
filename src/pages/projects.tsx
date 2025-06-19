38ade7148e2f539d22e9ffde225840abde30a7b1
Con-Adonis <conkavleski@gmail.com>
2025-06-19 18:33:49 -0400

    Project box information

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,17 +1,37 @@
 import Navbar from "@/components/navbar";
 
 export default function Projects() {
+  //Defining projects
+  const projects = [
+    //Project 1, SnapLang
+    {
+      id: 1,
+      name: "SnapLang",
+      image: "/images/SnapLang.png",
+      description: "An AI-powered flashcard builder using real-world objects.",
+    },
+  ]
+
+
   return (
     <>
       <Navbar />
       <main className="min-h-screen bg-white pt-20 px-4 sm:px-6 md:px-12">
         <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
-          {[1, 2, 3, 4].map((num) => (
+          {projects.map((project) => (
             <div
-              key={num}
-              className="bg-gray-200 rounded-lg w-full aspect-[4/3] max-w-sm flex justify-center items-center shadow-lg cursor-pointer transition hover:scale-105"
+              key = {project.id}
+              className = "relative bg-gray-100 rounded-lg overflow-hidden w-full aspect-[4/3] max-w-sm shadow-lg group cursor-pointer transition hover:scale-105 hover:shadow-xl"
             >
-              0{num}
+              <img
+                src = {project.image}
+                alt = {project.name}
+                className = "object-cover w-full h-full grouo-hover:scale-105 transition duration-300"
+              />
+
+              <div className="absolute bottom-0 left-0 w-full bg-black bg-opactiy-60 text-white p-4">
+                <h2 className="text-lg font-semibold">{project.name}</h2>
+              </div>
             </div>
           ))}
         </div>
